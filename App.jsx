936762d886968b0876ae0149efaefe2654eab8db
Kenguva Ashwini <panianu8@gmail.com>
2025-10-17 16:50:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header"
+import Boddy from "./components/body"
+import New_product from "./components/new_product"
+import Footer from "./components/footer"
+
+function App(){
+  return <div>
+    <Header/>
+    <Boddy/>
+    <New_product/>
+    <Footer/>
+
+  </div>
+}
+export default App
